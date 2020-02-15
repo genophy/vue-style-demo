7254051b9d5454af45eb84b1b86ca2b48f421f9b
genophy <genophy@gmail.com>
2020-02-16 05:37:11 +0800

    commit

---
+++ public/template-component/CSS.ColorfulHeartsTrailAnimation.vue
@@ -0,0 +1,102 @@
+<!--
+    FileDesc  :
+    Author    :  g_eno_phy
+    Date      :  2020-02-16 04:30
+    Version   :
+    Usage     :
+        - template
+            <CSS.GlowingLoadingText></CSS.GlowingLoadingText>
+
+        - props
+            :hello-world        {String}        你好世界
+        - event
+            @on-value-changed   {String}        值改变
+        - method
+            changeValue         {String}        改变值
+-->
+
+<template>
+    <div class="root" ref="root" @mousemove="handleMouseMove">
+
+    </div>
+</template>
+
+<script>
+import CommonsUtil from '@/libs/utils/commons.util';
+
+export default {
+    name    : 'CSS.GlowingLoadingText',
+    props   : {},
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+    },
+    methods : {
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________ [ handle ] ____________________________________________________ */
+        handleMouseMove(e) {
+            const heart = document.createElement('span');
+            CommonsUtil.debounce('heart', () => {
+                const x = e.offsetX;
+                const y = e.offsetY;
+                heart.style.left = `${x}px`;
+                heart.style.top = `${y}px`;
+                const size = Math.random() * 50;
+                heart.style.width = heart.style.height = `${size + 20}px`;
+                this.$refs.root.append(heart);
+                setTimeout(() => {
+                    heart.remove();
+                }, 2000);
+            }, 5, true);
+
+        }
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________ [ public ] ____________________________________________________ */
+
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________ [ private: *,query,fetch,action,init ] ________________________ */
+
+    }
+};
+</script>
+<style scoped lang="less">
+.root {
+    position : relative;
+    height   : 100%;
+}
+
+.root /deep/ span {
+    position        : absolute;
+    display         : block;
+    background      : url('../../src/assets/img/heart.png');
+    background-size : cover;
+    width           : 100%;
+    height          : 100%;
+    pointer-events  : none;
+    transform       : translate(-50%, -50%);
+    animation       : animate 2s linear infinite;
+}
+
+@keyframes animate {
+    0% {
+        transform : translate(-50%, -50%);
+        opacity   : 1;
+        filter    : hue-rotate(0deg);
+    }
+    100% {
+        transform : translate(-50%, -1000%);
+        opacity   : 0;
+        filter    : hue-rotate(720deg);
+    }
+}
+</style>
---
+++ public/template-component/CSS.GlowingLoadingText.vue
@@ -0,0 +1,107 @@
+<!--
+    FileDesc  :
+    Author    :  g_eno_phy
+    Date      :  2020-02-16 04:30
+    Version   :
+    Usage     :
+        - template
+            <CSS.GlowingLoadingText></CSS.GlowingLoadingText>
+
+        - props
+            :hello-world        {String}        你好世界
+        - event
+            @on-value-changed   {String}        值改变
+        - method
+            changeValue         {String}        改变值
+-->
+
+<template>
+    <div class="root">
+        <h2>
+            <span v-for="(txt,idx) in 'Loading'" :key="idx">{{txt}}</span>
+            <span>...</span>
+        </h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name    : 'CSS.GlowingLoadingText',
+    props   : {},
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+    },
+    methods : {
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________ [ handle ] ____________________________________________________ */
+
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________ [ public ] ____________________________________________________ */
+
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________________________________________________________________________ */
+        /* _____________________ [ private: *,query,fetch,action,init ] ________________________ */
+
+    }
+};
+</script>
+<style scoped lang="less">
+.root {
+    font-family     : Consolas, serif;
+    display         : flex;
+    flex-flow       : row nowrap;
+    justify-content : center;
+    align-items     : center;
+    height          : 100%;
+}
+
+h2 {
+    color     : #000;
+    font-size : 6em;
+    display   : flex;
+}
+
+h2 span {
+    animation : animate 4s linear infinite;
+}
+
+.loopSpan(@i) when (@i > 0) {
+    h2 span:nth-child(@{i}) {
+        animation-delay : @i * .1s;
+    }
+    .loopSpan(@i - 1)
+}
+
+.loopSpan(8);
+
+
+@keyframes animate {
+    0%, 100% {
+        color       : #fff;
+        filter      : blur(2px);
+        text-shadow : 0 0 10px #00b3ff,
+        0 0 20px #00b3ff,
+        0 0 40px #00b3ff,
+        0 0 80px #00b3ff,
+        0 0 120px #00b3ff,
+        0 0 200px #00b3ff,
+        0 0 300px #00b3ff,
+        0 0 400px #00b3ff;
+    }
+    25%, 75% {
+        color       : #000;
+        filter      : blur(0);
+        text-shadow : none;
+    }
+}
+</style>
